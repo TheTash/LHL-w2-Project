1f4b0cffe3cf1283e0c2968bc1b216018abd9dd5
TheTash <t.kashi.92@gmail.com>
2018-01-24 01:16:19 +0000

    app.post runs req.body through generateRandomShortUrl function

--- express_server.js
+++ express_server.js
@@ -7,13 +7,15 @@ var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 
-  generateRandomString : function(length, chars) {
-    var result = '';
-    for (var i = length; i > 0; i--){
-      return result;
-    }
-  }, //generateRandomString(6,'0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+
 };
+var generateRandomShortURL = function(length, chars) {
+  var result = '';
+  for (var i = length; i > 0; i--){
+    return result += chars[Math.floor(Math.random() * chars.length)];
+  }
+};
+
 
 
 
@@ -39,7 +41,8 @@ app.get("/urls/new", function (req, res) {
   res.render("urls_new");
 });
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // debug statement to see POST parameters
+  var excessURL = ''
+  excessURL += generateRandomShortURL(req.body);
   res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 app.get('/urls/:id', function(req, res) {
